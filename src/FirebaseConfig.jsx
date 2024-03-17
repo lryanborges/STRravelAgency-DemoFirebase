94b7212c1a5a27e9d0c2a8cfdc00256df1b7c7e4
lryanborges <ryanborgeslockser@gmail.com>
2024-03-17 13:08:58 -0300

    add: firebase gratuito reserva

--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -2,6 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseApp = initializeApp ({
+  apiKey: "AIzaSyCsaEPuPqPfFZTlrn87DVwXpkQm8inNP20",
+  authDomain: "reservademofirebase.firebaseapp.com",
+  projectId: "reservademofirebase",
+  storageBucket: "reservademofirebase.appspot.com",
+  messagingSenderId: "738435659456",
+  appId: "1:738435659456:web:e0863b9e892cb836df9f25",
+  measurementId: "G-LRLCQ9QFSY"
+});
+
+/*
+const firebaseConfig = {
     apiKey: "AIzaSyArKTbau_OS090sQ9T20FkQbCyzoPhSHSo",
     authDomain: "demofirebase-str.firebaseapp.com",
     projectId: "demofirebase-str",
@@ -9,6 +20,8 @@ const firebaseApp = initializeApp ({
     messagingSenderId: "324146414088",
     appId: "1:324146414088:web:5079eed960996644a607bd",
     measurementId: "G-RRWE2NEFM1"
-  });
+};
+*/
+
 
   export {firebaseApp, getAnalytics};
